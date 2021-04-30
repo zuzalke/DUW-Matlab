f723769acb33486172d3a0730554e941e72b560c
zuzalke <83219941+zuzalke@users.noreply.github.com>
2021-04-30 17:29:31 +0200

    Add files via upload

--- Fi.m
+++ Fi.m
@@ -1,5 +1,6 @@
 function F=Fi(q,t);
-%ALERT PLIK NIEDOKONCZONY - BRAK ROWNAN DLA PAR POSTEPOWYCH!
+%ALERT PLIK NIEDOKONCZONY - patrz: pary postępowe
+%bazowane na str 39 i 40 skryptu DUW + prezentacji programów z zajęć
 
  temp = fopen('Dane/ParyObrotowe.txt', 'r');
  nobr = str2num(fgetl(temp));
@@ -23,21 +24,16 @@ function F=Fi(q,t);
         end
         k=k+2;
  end
- %NIE WIEM CO ROBIĘ I CZY TO JEST DOBRZE - NIEKSONCZONE (NIE WIEM, JAK
- %OPISAĆ WIĘZY PAR POSTĘPOWYCH)
+ %NIE WIEM CO ROBIĘ I CZY TO JEST DOBRZE - NIEKSONCZONE - problemy z: fi0
+ %oraz vj
+ %+ czy trzeba tu rozważać te przypadki if else?
  for m=1:npos
         i=3*PrismaticJoint(m,1);
         j=3*PrismaticJoint(m,2);
-        if i == 0
-            F(k+1,1)=
-            F(k+2,1)=
-        elseif j == 0
-            F(k+1,1)=
-            F(k+2,1)=
-        else
-            F(k+1,1)=
-            F(k+2,1)=
-        end
+        
+            F(k+1,1)=q(i)-q(j)-fi0;         %fi0 - wg skryptu to "stały kąt wzgl. między układami odniesienia członów, który ma być zachowany w trakcie ruchu członów tworzących parę kinematyczną"; co to ma dokładnie być w naszym przypadku - nie wiem ;-;
+            F(k+2,1)=(q(j-2:j-1,1)+Rot(q(j))*PrismaticJoint(m,5:6)-q(i-2:i-1,1)-Rot(q(i))*PrismaticJoint(m,3:4))'*Rot(q(j))*vj %vj to jakiś wektor, ale nie bardzo rozumiem, skąd się bierze
+        
         k=k+2;
  end
       
